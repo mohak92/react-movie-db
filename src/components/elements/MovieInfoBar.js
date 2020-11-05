89574586386439958256948cdcd752be92d54a45
mohak92 <tamhane.mohak@gmail.com>
2020-11-04 20:37:31 -0500

    render movie info styled component

--- src/components/elements/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar.js
@@ -1,5 +1,32 @@
 import React from 'react';
+import FontAwesome from 'react-fontawesome';
+import { calcTime, convertMoney } from '../../helpers';
 
-const MovieInfoBar = () => <div>Movie Info Bar</div>;
+import { StyledMovieInfoBar } from '../styles/StyledMovieInfoBar';
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+    <StyledMovieInfoBar>
+        <div className="movieinfobar-content">
+            <div className="movieinfobar-content-col">
+                <FontAwesome className="fa-time" name="clock-o" sixe="2x" />
+                <span className="movieinfobar-info">
+                    Running time: {calcTime(time)}
+                </span>
+            </div>
+            <div className="movieinfobar-content-col">
+                <FontAwesome className="fa-budget" name="money" sixe="2x" />
+                <span className="movieinfobar-info">
+                    Budget: {convertMoney(budget)}
+                </span>
+            </div>
+            <div className="movieinfobar-content-col">
+                <FontAwesome className="fa-revenue" name="ticket" sixe="2x" />
+                <span className="movieinfobar-info">
+                    Revenue: {convertMoney(revenue)}
+                </span>
+            </div>
+        </div>
+    </StyledMovieInfoBar>
+);
 
 export default MovieInfoBar;
