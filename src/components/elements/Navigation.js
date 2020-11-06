558c057304f87d3e71f83d6ba8adb4d238a74952
mohak92 <tamhane.mohak@gmail.com>
2020-11-05 23:13:39 -0500

    propType to validate data type

--- src/components/elements/Navigation.js
+++ src/components/elements/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from '@reach/router';
+import PropTypes from 'prop-types';
 
 import { StyledNavigation } from '../styles/StyledNavigation';
 
@@ -14,6 +15,10 @@ const Navigation = ({ movie }) => (
         </div>
     </StyledNavigation>
 )
+
+Navigation.propTypes = {
+    movie: PropTypes.string,
+}
 
 export default Navigation;
 
